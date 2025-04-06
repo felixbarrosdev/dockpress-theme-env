5eee6766f08504b4b748d9ccbad9481ceeba1830
Felix Barros <soyfelixbarros@gmail.com>
2025-04-06 14:02:38 -0300

    chore(changelog): agregar emojis y secciones personalizadas en changelog.config.js

---
+++ changelog.config.js
@@ -0,0 +1,13 @@
+// changelog.config.js
+module.exports = {
+    types: [
+      { type: 'feat', section: '✨ Features', emoji: '✨' },
+      { type: 'fix', section: '🐛 Bug Fixes', emoji: '🐛' },
+      { type: 'docs', section: '📝 Documentation', emoji: '📝' },
+      { type: 'style', section: '💄 Style', emoji: '💄' },
+      { type: 'refactor', section: '♻️ Refactoring', emoji: '♻️' },
+      { type: 'perf', section: '⚡ Performance', emoji: '⚡' },
+      { type: 'test', section: '✅ Tests', emoji: '✅' },
+      { type: 'chore', section: '🔧 Chores', emoji: '🔧' }
+    ]
+  }
